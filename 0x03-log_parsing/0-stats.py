d3d91abcb1182cd17c42449046ea7b40510dadf9
baye94 <fallcheikh893@gmail.com>
2023-01-19 22:46:34 +0000

    stats

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,37 +1,35 @@
 #!/usr/bin/python3
-'''a script that reads stdin line by line and computes metrics'''
-
-
+""" 0. Log parsing """
 import sys
 
-cache = {'200': 0, '301': 0, '400': 0, '401': 0,
-         '403': 0, '404': 0, '405': 0, '500': 0}
+
+statuses = ["200", "301", "400", "401", "403", "404", "405", "500"]
+
+
+def print_stats(total_size, status_codes):
+    """ Prints a log stats. """
+    print(f"File size: {total_size}")
+    for status, count in sorted(status_codes.items()):
+        print(f"{status}: {count}")
+
+
 total_size = 0
-counter = 0
+status_codes = dict()
+count = 0
 
 try:
     for line in sys.stdin:
-        line_list = line.split(" ")
-        if len(line_list) > 4:
-            code = line_list[-2]
-            size = int(line_list[-1])
-            if code in cache.keys():
-                cache[code] += 1
-            total_size += size
-            counter += 1
+        fields = line.split()[::-1]     # reverse split log
 
-        if counter == 10:
-            counter = 0
-            print('File size: {}'.format(total_size))
-            for key, value in sorted(cache.items()):
-                if value != 0:
-                    print('{}: {}'.format(key, value))
+        if count == 10:
+            print_stats(total_size, status_codes)
+            count = 0
+            continue
 
-except Exception as err:
-    pass
-
+        total_size += int(fields[0])
+        status = fields[1]
+        if status in statuses:
+            status_codes[status] = status_codes.get(status, 0) + 1
+        count += 1
 finally:
-    print('File size: {}'.format(total_size))
-    for key, value in sorted(cache.items()):
-        if value != 0:
-            print('{}: {}'.format(key, value))
+    print_stats(total_size, status_codes)

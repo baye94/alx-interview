50abb287a8fd98aaae06195aff50bcfb7c29c439
baye94 <fallcheikh893@gmail.com>
2023-01-20 17:45:16 +0000

    fix bugs

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,37 +1,59 @@
 #!/usr/bin/python3
-'''a script that reads stdin line by line and computes metrics'''
+"""
+Module to parse a log file for statistics
+The stdin filestream is read for inputs and metrics are
+computed by python
+"""
 
-
+import re
 import sys
 
-cache = {'200': 0, '301': 0, '400': 0, '401': 0,
-         '403': 0, '404': 0, '405': 0, '500': 0}
-total_size = 0
-counter = 0
 
+filesize = 0
+errors = ["200", "301", "400", "401", "403", "404", "405", "500"]
+error_count = {}
+ip_re = "\b([1-9]|[1-9][0-9]|[1][0-9][0-9]|[2][0-4][0-9]|[2][5][0-5])\b\
+         .\b([1-9]|[1-9][0-9]|[1][0-9][0-9]|[2][0-4][0-9]|[2][5][0-5])\b\
+         .\b([1-9]|[1-9][0-9]|[1][0-9][0-9]|[2][0-4][0-9]|[2][5][0-5])\b\
+         .\b([1-9]|[1-9][0-9]|[1][0-9][0-9]|[2][0-4][0-9]|[2][5][0-5])\b\
+          - "
+datetime_regex = "[\b([1-9][0-9][0-9][0-9])\b-\b([0-9][0-9])\b\
+                  -\b([0-9][0-9])\b \b([0-9][0-9])\b:\b([0-9][0-9])\b:\b\
+                  ([0-9][0-9])\b.\b([0-9][0-9][0-9][0-9][0-9][0-9])\b] "
+url_regex = "\"GET /projects/260 HTTP/1.1\" "
+err_regex = "\b([2][0][0]|[3][0][1]|[4][0][0]|[4][0][1]|[4][0][3]|\
+             [4][0][4]|[4][0][5]|[5][0][0])\b "
+size_regex = "\b([1-9]|[1-9][0-9]|[1-9][0-9][0-9]|[1][0][0-2][0-4])\b"
+line_regex = re.compile(r''.format(ip_re + datetime_regex +
+                        url_regex + err_regex + size_regex))
 try:
+    i = 0
     for line in sys.stdin:
-        line_list = line.split(" ")
-        if len(line_list) > 4:
-            code = line_list[-2]
-            size = int(line_list[-1])
-            if code in cache.keys():
-                cache[code] += 1
-            total_size += size
-            counter += 1
-
-        if counter == 10:
-            counter = 0
-            print('File size: {}'.format(total_size))
-            for key, value in sorted(cache.items()):
-                if value != 0:
-                    print('{}: {}'.format(key, value))
-
-except Exception as err:
-    pass
-
-finally:
-    print('File size: {}'.format(total_size))
-    for key, value in sorted(cache.items()):
-        if value != 0:
-            print('{}: {}'.format(key, value))
+        is_valid = line_regex.search(line)
+        if is_valid:
+            i += 1
+            stats = line.split(' ')
+            try:
+                filesize += int(stats[-1])
+                if error_count.get(stats[-2]):
+                    error_count[stats[-2]] += 1
+                else:
+                    error_count[stats[-2]] = 1
+                if (i % 10 == 0):
+                    print('File size: {}'.format(filesize))
+                    for err in errors:
+                        if error_count.get(err):
+                            print('{}: {}'.format(err, error_count.get(err)))
+            except Exception:
+                pass
+except KeyboardInterrupt:
+    print('File size: {}'.format(filesize))
+    for err in errors:
+        if error_count.get(err):
+            print('{}: {}'.format(err, error_count.get(err)))
+    raise
+else:
+    print('File size: {}'.format(filesize))
+    for err in errors:
+        if error_count.get(err):
+            print('{}: {}'.format(err, error_count.get(err)))

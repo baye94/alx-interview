999e6f7bbac4998a9a2f2e6c5b73c68905522c75
baye94 <fallcheikh893@gmail.com>
2023-03-09 13:27:53 +0000

    prime game

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,48 +1,56 @@
 #!/usr/bin/python3
-"""
-Prime game DSA
-"""
+
+""" Prime Game Algorithm Python """
 
 
-def isPrime(num):
-    """Function to determine if a number is prime or not"""
-    if num == 1:
-        return False
-    for i in range(2, num):
-        if num % i == 0:
+def is_prime(n):
+    """ Checks if a number given n is a prime number """
+    for i in range(2, int(n ** 0.5) + 1):
+        if not n % i:
             return False
     return True
 
 
+def calculate_primes(n, primes):
+    """ Calculate all primes """
+    top_prime = primes[-1]
+    if n > top_prime:
+        for i in range(top_prime + 1, n + 1):
+            if is_prime(i):
+                primes.append(i)
+            else:
+                primes.append(0)
+
+
 def isWinner(x, nums):
-    """Function to determine the winner of a game"""
-    if x > len(nums):
-        return None
-    Maria = 0
-    Ben = 0
+    """
+    x is the number of rounds and nums is an array of n
+    Return: name of the player that won the most rounds
+    If the winner cannot be determined, return None
+    You can assume n and x will not be larger than 10000
+    """
+
+    players_wins = {"Maria": 0, "Ben": 0}
+
+    primes = [0, 0, 2]
+
+    calculate_primes(max(nums), primes)
+
     for round in range(x):
-        turn = 1
-        options = [n + 1 for n in range(nums[round])]
-        i = 1
-        while i < len(options):
-            if isPrime(options[i]):
-                div = options[i]
-                options.pop(i)
-                j = 0
-                while j < len(options):
-                    if options[j] % div == 0:
-                        options.pop(j)
-                    j += 1
-                turn += 1
-                i = 0
-            i += 1
-        if turn % 2 == 0:
-            Maria += 1
+        sum_options = sum((i != 0 and i <= nums[round])
+                          for i in primes[:nums[round] + 1])
+
+        if (sum_options % 2):
+            winner = "Maria"
         else:
-            Ben += 1
-    if Ben > Maria:
-        return 'Ben'
-    elif Maria > Ben:
-        return 'Maria'
-    else:
-        return None
+            winner = "Ben"
+
+        if winner:
+            players_wins[winner] += 1
+
+    if players_wins["Maria"] > players_wins["Ben"]:
+        return "Maria"
+    elif players_wins["Ben"] > players_wins["Maria"]:
+        return "Ben"
+
+    return None

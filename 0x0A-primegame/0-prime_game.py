1bbfda39cb60427367a01537e2470ffc94d9cab8
baye94 <fallcheikh893@gmail.com>
2023-03-09 12:58:13 +0000

    prime game

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,48 +1,48 @@
-#!/usr/bin/python3
-"""
-Prime game DSA
-"""
-
-
-def isPrime(num):
-    """Function to determine if a number is prime or not"""
-    if num == 1:
-        return False
-    for i in range(2, num):
-        if num % i == 0:
-            return False
-    return True
-
-
 def isWinner(x, nums):
-    """Function to determine the winner of a game"""
-    if x > len(nums):
-        return None
-    Maria = 0
-    Ben = 0
-    for round in range(x):
+    # function to generate a list of prime numbers up to n
+    def generate_primes(n):
+        primes = [True] * (n+1)
+        primes[0] = primes[1] = False
+        for i in range(2, int(n**0.5)+1):
+            if primes[i]:
+                for j in range(i**2, n+1, i):
+                    primes[j] = False
+        return [i for i in range(2, n+1) if primes[i]]
+
+    maria_wins = ben_wins = 0
+    for n in nums:
+        primes = generate_primes(n)
         turn = 1
-        options = [n + 1 for n in range(nums[round])]
-        i = 1
-        while i < len(options):
-            if isPrime(options[i]):
-                div = options[i]
-                options.pop(i)
-                j = 0
-                while j < len(options):
-                    if options[j] % div == 0:
-                        options.pop(j)
-                    j += 1
-                turn += 1
-                i = 0
-            i += 1
-        if turn % 2 == 0:
-            Maria += 1
-        else:
-            Ben += 1
-    if Ben > Maria:
-        return 'Ben'
-    elif Maria > Ben:
-        return 'Maria'
+        while primes:
+            # Maria's turn
+            if turn == 1:
+                for prime in primes:
+                    if n % prime == 0:
+                        primes.remove(prime)
+                        for i in range(prime, n+1, prime):
+                            if i in primes:
+                                primes.remove(i)
+                        break
+                else:
+                    ben_wins += 1
+                    break
+            # Ben's turn
+            else:
+                for prime in primes:
+                    if n % prime == 0:
+                        primes.remove(prime)
+                        for i in range(prime, n+1, prime):
+                            if i in primes:
+                                primes.remove(i)
+                        break
+                else:
+                    maria_wins += 1
+                    break
+            turn = 3 - turn
+
+    if maria_wins > ben_wins:
+        return "Maria"
+    elif ben_wins > maria_wins:
+        return "Ben"
     else:
         return None

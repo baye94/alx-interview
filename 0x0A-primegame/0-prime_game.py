e008bdb60d7823e63f9a563e4388ddf23110ac86
baye94 <fallcheikh893@gmail.com>
2023-03-09 13:21:41 +0000

    prime game

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -3,13 +3,12 @@
 Prime game DSA
 """
 
-import math
 
 def isPrime(num):
     """Function to determine if a number is prime or not"""
     if num == 1:
         return False
-    for i in range(2, int(math.sqrt(num))+1):
+    for i in range(2, num):
         if num % i == 0:
             return False
     return True
